38b3be3d9939c6008b70c7326675b0a995681201
Dorcas Familusi <63154615+thisisdorcas@users.noreply.github.com>
2022-07-13 10:25:06 +0000

    Attempted Challenge 1 && 2

--- src/assignment.js
+++ src/assignment.js
@@ -13,10 +13,23 @@ const assignment = {};
  * @param {Array} arrayOfNumbers the array of numbers to sum
  * @returns number the sum of the numbers
  */
+
+
 function sumOfNumbers(arrayOfNumbers) {
-    return arrayOfNumbers.length;
+    return arrayOfNumbers.reduce(function(number, value){
+        return number + value
+    },0);
 }
+
+console.log(sumOfNumbers(arrayOfNumbers))
+
+
 // assignment.sumOfNumbers = sumOfNumbers;
+
+
+
+
+
 
 /**
  * Challenge - 2
@@ -28,9 +41,22 @@ function sumOfNumbers(arrayOfNumbers) {
  * @returns number the count of even numbers
  */
 function countEvenNumbers(arrayOfNumbers) {
-    return arrayOfNumbers.length;
+  evenNumbers = arrayOfNumbers.filter((number) =>
+    (number % 2) === 0)
+
+  return evenNumbers.length
 }
+
+console.log(countEvenNumbers(arrayOfNumbers))
 // assignment.countEvenNumbers = countEvenNumbers;
+
+
+
+
+
+
+
+
 
 /**
  * Challenge - 3
